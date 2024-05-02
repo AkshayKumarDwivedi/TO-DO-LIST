9df4e0a18d9c398bbd735e77e642b51be9298da5
Akshay Dwivedi <118422874+AkshayKumarDwivedi@users.noreply.github.com>
2024-05-02 22:57:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+function addTask(){
+  if(inputBox.value===''){
+        alert("You must write something");
+  }
+  else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";//cross icon
+        li.appendChild(span);
+  }
+  inputBox.value=" ";//add task
+  savedData();
+}
+listContainer.addEventListener("click",function(e){
+        if(e.target.tagName==="LI"){
+                e.target.classList.toggle("checked");
+                savedData();
+        }
+        else if(e.target.tagName==="SPAN"){
+                e.target.parentElement.remove();//parent is LI
+                savedData();
+        }
+
+},false);
+function savedData(){
+        localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+        listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
